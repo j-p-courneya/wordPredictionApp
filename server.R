730cab6f37bf355ee899cd282332ba110d426a9e
Jean-Paul Courneya <jcourn1@gmail.com>
2018-05-15 22:50:09 -0400

    Preliminary development of app. Skeleton frame to build the app.

---
+++ server.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(dplyr)
+library(quanteda)
+library(data.table)
+
+shinyServer(
+  function(input,output) {
+    
+    #Display text user provided
+    txtReturn <- eventReactive(input$button1, {
+      input$impText
+    })
+    output$inputText <- renderText({txtReturn()})
+    
+    #Get a table of predicted words and score
+    #predWords <- eventReactive(input$button1, {
+    #stupidBackoffPredFunction(input$impText, input$impWords)
+    #})
+    #output$predTable <- renderTable({predWords()})
+  }
+)
